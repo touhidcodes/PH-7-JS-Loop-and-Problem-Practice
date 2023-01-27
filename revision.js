1c4c54958a1c2f5721b41e59d21888718402fcaf
Touhidur Zaman <touhidcodes@gmail.com>
2023-01-28 01:22:49 +0600

    revision

---
+++ revision.js
@@ -0,0 +1,21 @@
+var id = 24;
+var myName = "Touhidur Zaman";
+var arr = [12, 24, 45, 67, 56];
+arr.push(34);
+arr.pop();
+arr.unshift(12);
+arr.shift();
+
+if (myName == "Touhidur Zaman") {
+	console.log("True");
+} else {
+	console.log("False");
+}
+
+if (id == 24) {
+	console.log(24);
+} else if (id == 50) {
+	console.log(50);
+} else {
+	console.log("Wrong");
+}
