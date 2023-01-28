6956c6d770a109495138b1e255de2185c8cd49b9
Touhidur Zaman <touhidcodes@gmail.com>
2023-01-28 14:00:46 +0600

    array-loop

---
+++ array-loop.js
@@ -0,0 +1,13 @@
+var numbers = [23, 45, 656, 78, 67];
+
+for (let i = 0; i < numbers.length; i++) {
+	const number = numbers[i];
+	console.log(number);
+}
+
+var items = ["pen", "phone", "pad", "notebook"];
+
+for (let i = 0; i < items.length; i++) {
+	const item = items[i];
+	console.log(item);
+}
