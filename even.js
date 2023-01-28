c1944c2c1b73dd1e9a6e75cf039aa6ff729d4bf8
Touhidur Zaman <touhidcodes@gmail.com>
2023-01-28 12:38:40 +0600

    for

--- even.js
+++ even.js
@@ -1,6 +1,13 @@
-// all even numbers
+// target: all 0 to 10 even numbers
+
+// while loop
 var number = 0;
 while (number <= 10) {
 	console.log(number);
 	number = number + 2;
+}
+
+// for loop
+for (let i = 0; i < 10; i += 2) {
+	console.log(i);
 }
--- odd.js
+++ odd.js
@@ -1,6 +1,13 @@
-//  all odd numbers
+// target: all 1 to 10 odd numbers
+
+// while loop
 var number = 1;
 while (number <= 10) {
 	console.log(number);
 	number = number + 2;
+}
+
+// for loop
+for (let i = 1; i < 10; i += 2) {
+	console.log(i);
 }
